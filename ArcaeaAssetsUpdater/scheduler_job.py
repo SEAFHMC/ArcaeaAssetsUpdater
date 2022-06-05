254d35c54d7e27a61e02dddee477ee987b458c87
SEAFHMC <d1366160279@gmail.com>
2022-06-05 23:04:21 +0800

    timezone="Asia/Shanghai"

--- ArcaeaAssetsUpdater/scheduler_job.py
+++ ArcaeaAssetsUpdater/scheduler_job.py
@@ -9,10 +9,10 @@
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from assets_updater import ArcaeaAssetsUpdater
 
-scheduler = AsyncIOScheduler()
+scheduler = AsyncIOScheduler(timezone="Asia/Shanghai")
 
 
-@scheduler.scheduled_job("cron", hour=8, minute=15, timezone="Asia/Shanghai")
+@scheduler.scheduled_job("cron", hour=8, minute=15)
 async def _():
     if await ArcaeaAssetsUpdater.download_file():
         await ArcaeaAssetsUpdater.unzip_file()
